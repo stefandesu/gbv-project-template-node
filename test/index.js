56e19fa9f10cc034bbf3441a9fcc449723acb19c
Stefan Peters <stefandesu@exo.pm>
2020-01-23 12:39:36 +0900

    First commit

---
+++ test/index.js
@@ -0,0 +1,13 @@
+const assert = require("assert")
+
+describe("Basic Mocha String Test", () => {
+
+  it("should return number of charachters in a string", () => {
+    assert.equal("Hello".length, 5)
+  })
+
+  it("should return first charachter of the string", () => {
+    assert.equal("Hello".charAt(0), "H")
+  })
+
+})
